63c4a56d2562cede96ac017885554030f4883e47
Javid Arkishiev <javidarkishiev@gmail.com>
2024-12-18 18:01:38 +0400

    add feign client url

--- src/main/java/com/example/walletms/client/UserClient.java
+++ src/main/java/com/example/walletms/client/UserClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "auth-wallet-ms", url = "http://localhost:9090", configuration = FeignClientInterceptor.class)
+@FeignClient(name = "auth-wallet-ms", url = "https://auth-wallet-ms-6b5e5d2763f7.herokuapp.com", configuration = FeignClientInterceptor.class)
 public interface UserClient {
 
     @GetMapping("/users/profile/{phoneNumber}")
